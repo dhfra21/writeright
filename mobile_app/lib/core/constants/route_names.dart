98131bcde04e54e7966baeab225a880bfed82c23
Khira <Khira.hamdi@medtech.tn>
2026-03-31 03:40:04 +0100

    first commit

--- mobile_app/lib/core/constants/route_names.dart
+++
@@ -1,7 +0,0 @@
-class RouteNames {
-  static const String home = '/';
-  static const String practice = '/practice';
-  static const String parentLogin = '/parent-login';
-  static const String parentRegister = '/parent-register';
-  static const String createChild = '/create-child';
-}
